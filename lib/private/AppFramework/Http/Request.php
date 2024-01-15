a61844af5db444a5de00244cb63748a4895b116c
Ferdinand Thiessen <opensource@fthiessen.de>
2024-01-15 13:16:15 +0100

    fix(Request): Catch exceptions in `isTrustedProxy`
    The function fails if the configured trusted proxies contain invalid characters and the underlying IpUtils will throw.
    But as it is used by `getRemoteAddress` which is used by logging / templating, thrown errors are not reported but silently fail with error 500.
    
    Signed-off-by: Ferdinand Thiessen <opensource@fthiessen.de>

--- lib/private/AppFramework/Http/Request.php
+++ lib/private/AppFramework/Http/Request.php
@@ -573,7 +573,13 @@ class Request implements \ArrayAccess, \Countable, IRequest {
 	 * @return boolean true if $remoteAddress matches any entry in $trustedProxies, false otherwise
 	 */
 	protected function isTrustedProxy($trustedProxies, $remoteAddress) {
-		return IpUtils::checkIp($remoteAddress, $trustedProxies);
+		try {
+			return IpUtils::checkIp($remoteAddress, $trustedProxies);
+		} catch (\Throwable) {
+			// We can not log here as the logger is using `getRemoteAddress` which uses the function, so we would have a cyclic dependency
+			// Reaching this line means `trustedProxies` is in invalid format.
+			return false;
+		}
 	}
 
 	/**
